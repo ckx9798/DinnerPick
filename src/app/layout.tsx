7eaae6f5cddf94193036566958883905a2912ad4
MJ <ckx9798@gmail.com>
2025-08-12 07:22:35 +0900

    Feat: 네비바 구현 및 레이아웃 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+/** @format */
+
 import "./globals.css";
+
+import { Geist, Geist_Mono } from "next/font/google";
+
+import type { Metadata } from "next";
+import Navbar from "@/shared/ui/Navibar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
