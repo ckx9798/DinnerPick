bc81887b23f2e2b876fb2c230b08f2ef18dabb42
MJ <ckx9798@gmail.com>
2025-08-12 08:56:39 +0900

    Feat: 이미지 및 패키지 설치 반영

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,14 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "via.placeholder.com",
+        port: "",
+        pathname: "/**", // via.placeholder.com 의 모든 경로 허용
+      },
+      {
+        protocol: "https",
+        hostname: "i.namu.wiki",
+        port: "",
+        pathname: "/i/**", // i.namu.wiki 의 /i/ 로 시작하는 모든 경로 허용
       },
     ],
   },
