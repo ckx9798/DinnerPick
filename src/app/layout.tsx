17847648f6b6d01be489a4ddc3b76c2d71e53a69
MJ <ckx9798@gmail.com>
2025-08-13 04:19:50 +0900

    Feat: 구글 애널리틱스 연결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import type { Metadata } from "next";
 import Navbar from "@/shared/ui/Navibar";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,7 +43,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "오늘 뭐 먹지?",
     description: "점메추 저메추 해드립니다!",
-    // url: "https://your-domain.com",
+    url: "https://momuk.space",
     siteName: "MoMuk",
     images: [
       {
@@ -69,6 +70,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `}
+        </Script>
+
         <Navbar />
         {children}
       </body>
