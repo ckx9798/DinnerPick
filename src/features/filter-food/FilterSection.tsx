4466a68e66b1e9e40ab845f497258995caf95a33
MJ <ckx9798@gmail.com>
2025-08-14 21:27:03 +0900

    Feat: 상세조건 모달 컴포넌트 분리

--- src/features/filter-food/FilterSection.tsx
+++ src/features/filter-food/FilterSection.tsx
@@ -1,10 +1,9 @@
 /** @format */
 
-import { AnimatePresence, motion } from "framer-motion";
-import { ChevronDownIcon, XCircleIcon } from "@heroicons/react/24/solid";
-
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import FilterModal from "./FilterModal";
 import { categoryMap } from "@/shared/types/type";
-import { personalFilter } from "@/shared/types/constants";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 interface FilterSectionProps {
@@ -24,11 +23,6 @@ export default function FilterSection({
 }: FilterSectionProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    onDetailedSubmit(e);
-    setIsModalOpen(false);
-  };
-
   return (
     <>
       {/* --- 음식 카테고리 선택 영역 --- */}
@@ -36,12 +30,12 @@ export default function FilterSection({
         <h3 className="text-lg font-semibold mb-4 text-gray-700 text-center">
           음식 종류 선택
         </h3>
-        <div className="flex flex-wrap justify-center gap-3 md:gap-4">
+        <div className="grid grid-cols-3 md:grid-cols-3 gap-x-3 gap-y-2.5 md:gap-4 px-1">
           {Object.entries(categoryMap).map(([label, value]) => (
             <motion.button
               key={label}
               onClick={() => onFilterChange("category", value)}
-              className={`px-3 md:px-4 py-2 rounded-full font-medium transition-colors duration-200 text-xs md:text-sm ${
+              className={`w-full px-3 md:px-4 py-2 lg:py-3 rounded-2xl font-medium transition-colors duration-200 text-xs md:text-sm ${
                 (filters.category || "") === value
                   ? "bg-emerald-500 text-white shadow-md font-bold"
                   : "bg-gray-200 text-gray-700 hover:bg-gray-300"
@@ -69,86 +63,14 @@ export default function FilterSection({
       </motion.button>
 
       {/* --- 상세 조건 설정 모달 --- */}
-      <AnimatePresence>
-        {isModalOpen && (
-          <motion.div
-            className="fixed inset-0 bg-gray-100 bg-opacity-60 flex items-center justify-center p-4 z-50"
-            onClick={() => setIsModalOpen(false)}
-          >
-            <motion.div
-              initial={{ y: 50, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              exit={{ y: 50, opacity: 0 }}
-              className="bg-gray-50 p-6 rounded-2xl shadow-2xl w-full max-w-lg relative max-h-[90vh] overflow-y-auto"
-              onClick={(e) => e.stopPropagation()}
-            >
-              <button
-                type="button"
-                onClick={() => setIsModalOpen(false)}
-                className="absolute top-4 right-4 p-2 text-gray-500 rounded-full cursor-pointer"
-                aria-label="닫기"
-              >
-                <XCircleIcon className="w-6 h-6 -mr-2" />
-              </button>
-
-              <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
-                개인별 상세 조건 설정
-              </h2>
-              <form onSubmit={handleSubmit} className="space-y-6">
-                {/* --- 개인 조건 설정 --- */}
-                {personalFilter.map(({ title, key, options }) => (
-                  <div key={key}>
-                    <label className="block text-gray-700 font-semibold mb-2">
-                      {title}
-                    </label>
-                    <div className="flex flex-wrap gap-2">
-                      {options.map((opt) => (
-                        <label
-                          key={opt}
-                          className={`flex items-center justify-center space-x-2 w-22 py-1.5 md:py-2 rounded-xl border cursor-pointer text-sm font-medium ${
-                            filters[key] === opt
-                              ? "bg-emerald-500 text-white"
-                              : "bg-white text-gray-400"
-                          }`}
-                        >
-                          <input
-                            type="radio"
-                            name={key}
-                            value={opt}
-                            checked={filters[key] === opt}
-                            onChange={(e) =>
-                              onFilterChange(key, e.target.value)
-                            }
-                            className="hidden"
-                          />
-                          <span>{opt}</span>
-                        </label>
-                      ))}
-                    </div>
-                  </div>
-                ))}
-
-                {/* --- 하단 버튼 영역 --- */}
-                <div className="flex space-x-4 pt-4">
-                  <motion.button
-                    type="submit"
-                    className="flex-1 bg-emerald-500 text-white font-bold py-3 rounded-lg hover:bg-emerald-600"
-                  >
-                    적용하기
-                  </motion.button>
-                  <motion.button
-                    type="button"
-                    onClick={onResetFilters}
-                    className="flex-1 bg-gray-300 text-gray-800 font-bold py-3 rounded-lg hover:bg-gray-400"
-                  >
-                    초기화
-                  </motion.button>
-                </div>
-              </form>
-            </motion.div>
-          </motion.div>
-        )}
-      </AnimatePresence>
+      <FilterModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        filters={filters}
+        onFilterChange={onFilterChange}
+        onDetailedSubmit={onDetailedSubmit}
+        onResetFilters={onResetFilters}
+      />
     </>
   );
 }
